21de9efe1afbb2b37dc5a1295f44ea4ff88a5a18
AmelieHeinrich <amelie.heinrich.dev@gmail.com>
2025-09-28 20:02:31 +0200

    add: more work on metal fixes

--- code/kaleidoscope/KernelGPU-Metal/KGPU_MetalTextureView.mm
+++ code/kaleidoscope/KernelGPU-Metal/KGPU_MetalTextureView.mm
@@ -47,10 +47,7 @@ namespace KGPU
             case TextureViewDimension::kTexture2D:
                 return MTLTextureType2D;
             case TextureViewDimension::kTextureCube:
-                if (desc.Type == TextureViewType::kShaderWrite)
-                    return MTLTextureType2DArray;
-                else
-                    return MTLTextureTypeCube;
+                return MTLTextureTypeCube;
         }
     }
 }
