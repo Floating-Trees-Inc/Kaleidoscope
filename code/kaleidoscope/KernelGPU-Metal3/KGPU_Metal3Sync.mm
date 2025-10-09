6672caf17d23f0f2b5f4e03a986aef668688050a
AmelieHeinrich <amelie.heinrich.dev@gmail.com>
2025-10-09 23:47:40 +0200

    add: switched to zed

--- code/kaleidoscope/KernelGPU-Metal3/KGPU_Metal3Sync.mm
+++ code/kaleidoscope/KernelGPU-Metal3/KGPU_Metal3Sync.mm
@@ -33,29 +33,27 @@ namespace KGPU
 
     void Metal3Sync::PresentSurface()
     {
-        @autoreleasepool {
-            ITexture* currentSwapTexture = mSurface->GetTexture(mActualFrameIndex);
-            id<MTLTexture> srcTex = static_cast<Metal3Texture*>(currentSwapTexture)->GetMTLTexture();
+        ITexture* currentSwapTexture = mSurface->GetTexture(mActualFrameIndex);
+        id<MTLTexture> srcTex = static_cast<Metal3Texture*>(currentSwapTexture)->GetMTLTexture();
 
-            CAMetalLayer* layer = mSurface->GetLayer();
-            auto drawable = [layer nextDrawable];
-            id<MTLTexture> dstTex = [drawable texture];
+        CAMetalLayer* layer = mSurface->GetLayer();
+        auto drawable = [layer nextDrawable];
+        id<MTLTexture> dstTex = [drawable texture];
 
-            // New command buffer for the present pass
-            id<MTLCommandBuffer> buffer = [mQueue->GetMTLCommandQueue() commandBuffer];
-            id<MTLBlitCommandEncoder> ec = [buffer blitCommandEncoder];
-            [ec copyFromTexture:srcTex
-                     sourceSlice:0
-                     sourceLevel:0
-                     sourceOrigin:MTLOriginMake(0, 0, 0)
-                     sourceSize:MTLSizeMake(srcTex.width, srcTex.height, 1)
-                     toTexture:dstTex
-                     destinationSlice:0
-                     destinationLevel:0
-                     destinationOrigin:MTLOriginMake(0, 0, 0)];
-            [ec endEncoding];
-            [buffer presentDrawable:drawable];
-            [buffer commit];
-        }
+        // New command buffer for the present pass
+        id<MTLCommandBuffer> buffer = [mQueue->GetMTLCommandQueue() commandBuffer];
+        id<MTLBlitCommandEncoder> ec = [buffer blitCommandEncoder];
+        [ec copyFromTexture:srcTex
+                 sourceSlice:0
+                 sourceLevel:0
+                 sourceOrigin:MTLOriginMake(0, 0, 0)
+                 sourceSize:MTLSizeMake(srcTex.width, srcTex.height, 1)
+                 toTexture:dstTex
+                 destinationSlice:0
+                 destinationLevel:0
+                 destinationOrigin:MTLOriginMake(0, 0, 0)];
+        [ec endEncoding];
+        [buffer presentDrawable:drawable];
+        [buffer commit];
     }
 }
